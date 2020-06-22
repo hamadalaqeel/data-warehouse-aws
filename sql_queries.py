446e2fe7b390308f9331dd5129195a2ec97ecc7d
Hamad Alaqeel <h.alaqeel1996@gmail.com>
2020-06-22 11:19:08 +0300

    Use DISTINCT, add Primary Keys, INSERT NextSong.
     INSERT only rows where the page is NextSong.

--- sql_queries.py
+++ sql_queries.py
@@ -1,117 +1,108 @@
 import configparser
 
-import configparser
 
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
 # DROP TABLES
-staging_events_table_drop = "DROP TABLE IF EXISTS public.events_stage;"
-staging_songs_table_drop = "DROP TABLE IF EXISTS public.songs_stage;"
-songplay_table_drop = "DROP TABLE IF EXISTS public.songplays;"
-user_table_drop = "DROP TABLE IF EXISTS public.users;"
-song_table_drop = "DROP TABLE IF EXISTS public.songs;"
-artist_table_drop = "DROP TABLE IF EXISTS public.artists;"
-time_table_drop = "DROP TABLE IF EXISTS public.time;"
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-staging_events_table_create = ("""
-CREATE TABLE public.events_stage(
-    artist_id VARCHAR ENCODE ZSTD,
-    auth VARCHAR ENCODE ZSTD,
-    first_name VARCHAR ENCODE ZSTD,
-    gender VARCHAR ENCODE ZSTD,
-    item_in_session VARCHAR ENCODE ZSTD,
-    last_name VARCHAR ENCODE ZSTD,
-    length FLOAT8 ENCODE ZSTD,
-    level VARCHAR ENCODE ZSTD,
-    location VARCHAR ENCODE ZSTD,
-    method VARCHAR ENCODE ZSTD,
-    page VARCHAR ENCODE ZSTD,
-    registration VARCHAR ENCODE ZSTD,
-    session_id VARCHAR ENCODE ZSTD,
-    song_title VARCHAR ENCODE ZSTD,
-    status VARCHAR ENCODE ZSTD,
-    ts VARCHAR ENCODE ZSTD,
-    user_agent VARCHAR ENCODE ZSTD,
-    user_id VARCHAR ENCODE ZSTD
-);
+
+staging_events_table_create = ("""CREATE TABLE staging_events(
+    event_id INT IDENTITY(0,1),
+    artist_name VARCHAR(255),
+    auth VARCHAR(50),
+    user_first_name VARCHAR(255),
+    user_gender  VARCHAR(1),
+    item_in_session	INTEGER,
+    user_last_name VARCHAR(255),
+    song_length	DOUBLE PRECISION, 
+    user_level VARCHAR(50),
+    location VARCHAR(255),	
+    method VARCHAR(25),
+    page VARCHAR(35),	
+    registration VARCHAR(50),	
+    session_id	BIGINT,
+    song_title VARCHAR(255),
+    status INTEGER,  -- SHOULD BE ALWAYS A NUMBER, HTTP STATUS	
+    ts VARCHAR(50),
+    user_agent TEXT,	
+    user_id VARCHAR(100),
+    PRIMARY KEY (event_id))
 """)
 
-staging_songs_table_create = ("""
-CREATE TABLE public.songs_stage(
-    num_songs INT ENCODE ZSTD,
-    artist_id VARCHAR ENCODE ZSTD,
-    artist_latitude FLOAT8 ENCODE ZSTD,
-    artist_longitude FLOAT8 ENCODE ZSTD,
-    artist_location VARCHAR ENCODE ZSTD,
-    artist_name VARCHAR ENCODE ZSTD,
-    song_id VARCHAR ENCODE ZSTD,
-    title VARCHAR ENCODE ZSTD,
-    duration FLOAT8 ENCODE ZSTD,
-    year INT ENCODE ZSTD
-);
+staging_songs_table_create = ("""CREATE TABLE staging_songs(
+    song_id VARCHAR(100),
+    num_songs INTEGER,
+    artist_id VARCHAR(100),
+    artist_latitude DOUBLE PRECISION,
+    artist_longitude DOUBLE PRECISION,
+    artist_location VARCHAR(255),
+    artist_name VARCHAR(255),
+    title VARCHAR(255),
+    duration DOUBLE PRECISION,
+    year INTEGER,
+    PRIMARY KEY (song_id))
 """)
 
-songplay_table_create = ("""
-CREATE TABLE public.songplays(
-    songplay_id INT IDENTITY(0,1) PRIMARY KEY ENCODE ZSTD,
-    start_time TIMESTAMP NOT NULL ENCODE RAW,
-    user_id VARCHAR NOT NULL ENCODE RAW DISTKEY,
-    level VARCHAR ENCODE ZSTD,
-    song_id VARCHAR NOT NULL ENCODE ZSTD,
-    artist_id VARCHAR NOT NULL ENCODE ZSTD,
-    session_id VARCHAR ENCODE ZSTD,
-    location VARCHAR ENCODE ZSTD,
-    user_agent VARCHAR ENCODE ZSTD
-)
-SORTKEY(start_time);
+songplay_table_create = ( """CREATE TABLE songplays(
+    songplay_id INT IDENTITY(0,1),
+    start_time TIMESTAMP REFERENCES time(start_time),
+    user_id VARCHAR(50) REFERENCES users(user_id),
+    level VARCHAR(50),
+    song_id VARCHAR(100) REFERENCES songs(song_id),
+    artist_id VARCHAR(100) REFERENCES artists(artist_id),
+    session_id BIGINT,
+    location VARCHAR(255),
+    user_agent TEXT,
+    PRIMARY KEY (songplay_id))
 """)
 
-user_table_create = ("""
-CREATE TABLE public.users(
-    user_id VARCHAR PRIMARY KEY,
-    first_name VARCHAR ENCODE ZSTD,
-    last_name VARCHAR ENCODE ZSTD,
-    gender VARCHAR(1)ENCODE ZSTD,
-    level VARCHAR ENCODE ZSTD
-)
-DISTSTYLE AUTO
+user_table_create = ("""CREATE TABLE users(
+    user_id VARCHAR,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(1),
+    level VARCHAR(50),
+    PRIMARY KEY (user_id))
 """)
 
-song_table_create = ("""
-CREATE TABLE public.songs (
-    song_id VARCHAR PRIMARY KEY ENCODE ZSTD,
-    title VARCHAR NOT NULL ENCODE RAW,
-    artist_id VARCHAR NOT NULL DISTKEY ENCODE RAW,
-    year INT4 ENCODE ZSTD,
-    duration FLOAT8 ENCODE ZSTD
-)
-SORTKEY(title);
+song_table_create = ("""CREATE TABLE songs(
+    song_id VARCHAR(100),
+    title VARCHAR(255),
+    artist_id VARCHAR(100) NOT NULL,
+    year INTEGER,
+    duration DOUBLE PRECISION,
+    PRIMARY KEY (song_id))
 """)
 
-artist_table_create = ("""
-CREATE TABLE public.artists (
-    artist_id VARCHAR PRIMARY KEY ENCODE ZSTD,
-    name VARCHAR NOT NULL ENCODE ZSTD,
-    location VARCHAR ENCODE ZSTD,
-    lattitude FLOAT8 ENCODE ZSTD,
-    longitude FLOAT8 ENCODE ZSTD
-)
-DISTSTYLE AUTO;
+artist_table_create = ("""CREATE TABLE artists(
+    artist_id VARCHAR(100),
+    name VARCHAR(255),
+    location VARCHAR(255),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    PRIMARY KEY (artist_id))
 """)
 
-time_table_create = ("""
-CREATE TABLE public.time (
-    start_time TIMESTAMP PRIMARY KEY ENCODE ZSTD,
-    hour INT ENCODE ZSTD, 
-    day INT ENCODE ZSTD, 
-    week INT ENCODE ZSTD, 
-    month INT ENCODE ZSTD, 
-    year INT ENCODE ZSTD, 
-    weekday INT ENCODE ZSTD
-)DISTSTYLE AUTO;
+time_table_create = ("""CREATE TABLE time(
+    start_time TIMESTAMP,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
+    PRIMARY KEY (start_time))
 """)
 
 # STAGING TABLES
@@ -132,89 +123,76 @@ FORMAT AS JSON 'auto' REGION 'us-west-2' TRUNCATECOLUMNS;
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-INSERT INTO public.songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT 
-    TIMESTAMP 'epoch' + ts::INT8/1000 * INTERVAL '1 second' AS start_time,
-    e.user_id,
-    e.level,
-    s.song_id,
-    e.artist_id,
-    e.session_id,
-    e.location,
-    e.user_agent    
-FROM public.events_stage e
-LEFT JOIN public.songs_stage s
-ON e.song_title = s.title
-AND e.artist_id = s.artist_id
-WHERE page = 'NextSong';
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    SELECT DISTINCT 
+        TIMESTAMP 'epoch' + ts/1000 *INTERVAL '1 second' as start_time, 
+        e.user_id, 
+        e.user_level,
+        s.song_id,
+        s.artist_id,
+        e.session_id,
+        e.location,
+        e.user_agent
+    FROM staging_events e, staging_songs s
+    WHERE e.page = 'NextSong'
+    AND e.song_title = s.title
+    AND user_id NOT IN (SELECT DISTINCT s.user_id FROM songplays s WHERE s.user_id = user_id
+                       AND s.start_time = start_time AND s.session_id = session_id )
+""")
+ 
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)  
+    SELECT DISTINCT 
+        user_id,
+        user_first_name,
+        user_last_name,
+        user_gender, 
+        user_level
+    FROM staging_events
+    WHERE page = 'NextSong'
+    AND user_id NOT IN (SELECT DISTINCT user_id FROM users)
 """)
 
-# using the window function here in the CTE
-# enables us to filter and pick only the latest of the duplicate user entries
-user_table_insert = ("""
-INSERT INTO public.users(user_id, first_name, last_name, gender, level)
-WITH unique_user AS (
-    SELECT user_id,
-    first_name,
-    last_name,
-    gender,
-    level,
-    ROW_NUMBER() over (partition by user_id order by ts desc ) as index
-FROM public.events_stage
-)
-SELECT user_id,
-    first_name,
-    last_name,
-    gender,
-    level
-FROM unique_user
-WHERE COALESCE(user_id, '') <> '' and unique_user.index = 1
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
+    SELECT DISTINCT 
+        song_id, 
+        title,
+        artist_id,
+        year,
+        duration
+    FROM staging_songs
+    WHERE song_id NOT IN (SELECT DISTINCT song_id FROM songs)
 """)
 
-song_table_insert = ("""
-INSERT INTO public.songs(song_id, title, artist_id, year, duration)
-SELECT DISTINCT
-    song_id,
-    title,
-    artist_id,
-    year,    
-    duration
-FROM public.songs_stage;
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+    SELECT DISTINCT 
+        artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
+    FROM staging_songs
+    WHERE artist_id NOT IN (SELECT DISTINCT artist_id FROM artists)
 """)
 
-artist_table_insert = ("""
-INSERT INTO public.artists(artist_id, name, location, lattitude, longitude)
-SELECT DISTINCT
-    artist_id,
-    artist_name,
-    artist_location,
-    artist_latitude,    
-    artist_longitude
-FROM public.songs_stage;
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    SELECT 
+        start_time, 
+        EXTRACT(hr from start_time) AS hour,
+        EXTRACT(d from start_time) AS day,
+        EXTRACT(w from start_time) AS week,
+        EXTRACT(mon from start_time) AS month,
+        EXTRACT(yr from start_time) AS year, 
+        EXTRACT(weekday from start_time) AS weekday 
+    FROM (
+    	SELECT DISTINCT  TIMESTAMP 'epoch' + ts/1000 *INTERVAL '1 second' as start_time 
+        FROM staging_events s     
+    )
+    WHERE start_time NOT IN (SELECT DISTINCT start_time FROM time)
 """)
 
-time_table_insert = ("""
-INSERT INTO public.time(start_time, hour, day, week, month, year, weekday)
-WITH time_parse AS
-(
-    SELECT
-        DISTINCT TIMESTAMP 'epoch' + ts::INT8/1000 * INTERVAL '1 second' AS start_time
-    FROM public.events_stage
-)
-SELECT
-    start_time AS start_time,
-    EXTRACT (hour from start_time) AS hour,
-    EXTRACT (day from start_time) AS day,
-    EXTRACT (week from start_time) AS week,
-    EXTRACT (month from start_time) AS month,
-    EXTRACT (year from start_time) AS year,
-    EXTRACT (dow from start_time) AS weekday
-FROM time_parse;
-""")
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
